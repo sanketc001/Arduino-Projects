0e3b5fcf053ca3bd559e975651fc09c5fcad378d
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-07-24 19:14:03 +0530

    Update Solid State Relay.ino

--- Solid State Relay.ino
+++ Solid State Relay.ino
@@ -1,178 +1,146 @@
-#include <ESP8266WiFi.h>//Includes ESP8266 Wifi library
-//Defines representation of the pins used for each relay
-#define Relay_01 16        //D0
-#define Relay_02 5         //D1
-#define Relay_03 4         //D2
-#define Relay_04 0         //D3
+#include "ESP8266WiFi.h"
+#include "ESPAsyncWebServer.h"
 
-//Create a server on port 80
-//(default port where browsers send http requests)
-WiFiServer server(80);
+// Set to true to define Relay as Normally Open (NO)
+#define RELAY_NO    true
 
-void setup()
-{
-  //Initializes Serial for logging purposes only
-  Serial.begin(115200);
+// Set number of relays
+#define NUM_RELAYS  4
 
-  Serial.print("inside void setup");
-  //Sets the mode of all pins
-  pinMode(Relay_01, OUTPUT);
-  pinMode(Relay_02, OUTPUT);
-  pinMode(Relay_03, OUTPUT);
-  pinMode(Relay_04, OUTPUT);
+// Assign each GPIO to a relay
+int relayGPIOs[NUM_RELAYS] = {5,4,14,12};
 
-  //Adjust the state of all pins
-  digitalWrite(Relay_01, HIGH);
-  digitalWrite(Relay_02, HIGH);
-  digitalWrite(Relay_03, HIGH);
-  digitalWrite(Relay_04, HIGH);
+// Replace with your network credentials
+const char* ssid = "Pred@tor Gaming";//Pred@tor Gaming
+const char* password = "19111998";//19111998
 
-  Serial.print("Connected....!");//reporting current state (debug)
-  //Makes the ESP connect to the WiFi network available at the place of use.
-  //In our example the ssid of the network is networkWiFi and the password is passwordtest
-  WiFi.begin("Pred@tor Gaming", "19111998");
+const char* PARAM_INPUT_1 = "relay";  
+const char* PARAM_INPUT_2 = "state";
 
-  // As long as the ESP does not connect to the network
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    //We wait 100 milliseconds
-    delay(100);
-    Serial.print(".");
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+    input:checked+.slider {background-color: #2196F3}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?relay="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?relay="+element.id+"&state=0", true); }
+  xhr.send();
+}</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    for(int i=1; i<=NUM_RELAYS; i++){
+      String relayStateValue = relayState(i);
+      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+    }
+    return buttons;
   }
-
-  //If you arrived here, it's because you connected to the network,
-  //then we show it on the serial monitor to have a feedback
-  Serial.println("");
-  Serial.println("connected!!");
-
-  //Fixed IP settings. You can change it according to your network
-  Serial.println(WiFi.localIP());
-  //send the configuration
-  //iFi.config(ip, gateway, subnet);
-
-  //Initialize the server we created on port 80
-  server.begin();
-
-  //We show on the serial monitor the IP that the ESP has
-  //to check if it is the same as the one we configured
-  Serial.print("server on: ");
-  Serial.println(WiFi.localIP());
+  return String();
+}
+String relayState(int numRelay){
+  if(RELAY_NO){
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "";
+    }
+    else {
+      return "checked";
+    }
+  }
+  else {
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "checked";
+    }
+    else {
+      return "";
+    }
+  }
+  return "";
 }
 
-void loop()
-{
-  //Checks if any clients are trying to connect
-  WiFiClient client = server.available();
-  if (!client)
-  {
-    //If there is no customer we can return it as there is nothing to do
-    return;
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  // Set all relays to off when the program starts - if set to Normally Open (NO), the relay is off when you set the relay to HIGH
+  for(int i=1; i<=NUM_RELAYS; i++){
+    pinMode(relayGPIOs[i-1], OUTPUT);
+    if(RELAY_NO){
+      digitalWrite(relayGPIOs[i-1], HIGH);
+    }
+    else{
+      digitalWrite(relayGPIOs[i-1], LOW);
+    }
   }
-  //We read the request
-  String req = client.readStringUntil('\r');
-  req.replace("GET /", "");
-  req.replace(" HTTP/1.1","");
-  //Serial.print("Request: ");
-  Serial.println(req);
-
-  //This is the html we will return to the client
-  //It basically consists of two buttons (ON and OFF) for each relay
-  //The part that interests us is the <a href=' with the action linked to each button
-  //When we click on one of these buttons this information will reach the ESP for
-  //let it check which action it should perform
-  //The part inside '<style>' is just for us to change the look of the page
-  //which will be displayed, you can change it however you want
-  String html =
-    "<html>"
-    "<head>"
-    "<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'/>"
-    "<title>Controle Esp8266</title>"
-    "<style>"
-    "body{"
-    "text-align: center;"
-    "font-family: sans-serif;"
-    "font-size:25px;"
-    "padding: 25px;"
-    "}"
-    "p{"
-    "color:#444;"
-    "}"
-    "button{"
-    "outline: none;"
-    "border: 2px solid #1fa3ec;"
-    "border-radius:18px;"
-    "background-color:#FFF;"
-    "color: #1fa3ec;"
-    "padding: 5px 25px;"
-    "}"
-    "button:active{"
-    "color: #fff;"
-    "background-color:#1fa3ec;"
-    "}"
-    "button:hover{"
-    "border-color:#0000ff;"
-    "}"
-    "</style>"
-    "</head>"
-    "<body>"
-    "<p>01 - "
-    "<a href='Relay_01On'><button>ON</button></a>"
-    "<a href='Relay_01Off'><button>OFF</button></a></p>"
-    "<p>02 - "
-    "<a href='Relay_02On'><button>ON</button></a>"
-    "<a href='Relay_02Off'><button>OFF</button></a></p>"
-    "<p>03 - "
-    "<a href='Relay_03On'><button>ON</button></a>"
-    "<a href='Relay_03Off'><button>OFF</button></a></p>"
-    "<p>04 - "
-    "<a href='Relay_04On'><button>ON</button></a>"
-    "<a href='Relay_04Off'><button>OFF</button></a></p>"
-    "</body>"
-    "</html>";
-  //Escreve o html no buffer que será enviado para o cliente
-  client.print(html);
-  //Envia os dados do buffer para o cliente
-  client.flush();
-
-  //A partir daqui, verificamos se a requisição possui algum comando de
-  //atuação sobre os relés
-  if (req.indexOf("Relay_01On") != -1)
-  {
-    digitalWrite(Relay_01, LOW);
-  }
-  else if (req.indexOf("Relay_01Off") != -1)
-  {
-    digitalWrite(Relay_01, HIGH);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
 
-  if (req.indexOf("Relay_02On") != -1)
-  {
-    digitalWrite(Relay_02, LOW);
-  }
-  else if (req.indexOf("Relay_02Off") != -1)
-  {
-    digitalWrite(Relay_02, HIGH);
-  }
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
 
-  if (req.indexOf("Relay_03On") != -1)
-  {
-    digitalWrite(Relay_03, LOW);
-  }
-  else if (req.indexOf("Relay_03Off") != -1)
-  {
-    digitalWrite(Relay_03, HIGH);
-  }
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
 
-  if (req.indexOf("Relay_04On") != -1)
-  {
-    digitalWrite(Relay_04, LOW);
-  }
-  else if (req.indexOf("Relay_04Off") != -1)
-  {
-    digitalWrite(Relay_04, HIGH);
-  }
-  delay(200);//waits for information traffic to complete
-  //Close the connection with the client
-  client.stop();
-  //Serial.println("Client disconnected");
+  // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    String inputMessage2;
+    String inputParam2;
+    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      inputParam2 = PARAM_INPUT_2;
+      if(RELAY_NO){
+        Serial.print("NO ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
+      }
+      else{
+        Serial.print("NC ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage + inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+  // Start server
+  server.begin();
+}
+void loop() {
 }
